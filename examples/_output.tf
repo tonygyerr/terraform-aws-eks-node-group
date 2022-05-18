73fcc43309b63bab6d1cba95ce5e36d3417b4062
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2022-05-18 15:37:57 -0400

    eks-node-group

--- examples/_output.tf
+++ examples/_output.tf
@@ -1,9 +1,24 @@
-output "eks_managed_node_groups" {
-  description = "Map of attribute maps for all EKS managed node groups created"
-  value       = module.eks.eks_managed_node_groups
+output "node_group_arn" {
+  description = "Amazon Resource Name (ARN) of the EKS Node Group"
+  value       = module.eks_managed_node_group.node_group_arn
 }
 
-output "eks_managed_node_groups_autoscaling_group_names" {
-  description = "List of the autoscaling group names created by EKS managed node groups"
-  value       = module.eks.eks_managed_node_groups_autoscaling_group_names
+output "node_group_id" {
+  description = "EKS Cluster name and EKS Node Group name separated by a colon (`:`)"
+  value       = module.eks_managed_node_group.node_group_id
+}
+
+output "node_group_resources" {
+  description = "List of objects containing information about underlying resources"
+  value       = module.eks_managed_node_group.node_group_resources
+}
+
+output "node_group_autoscaling_group_names" {
+  description = "List of the autoscaling group names"
+  value       = module.eks_managed_node_group.node_group_autoscaling_group_names
+}
+
+output "node_group_status" {
+  description = "Status of the EKS Node Group"
+  value       = module.eks_managed_node_group.node_group_status
 }
